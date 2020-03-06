d8832ed8954b59d8447a03a02a5b47132cd7ca55
premti <premti@gmail.com>
2020-03-06 15:33:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,7 +31,6 @@ def get_japanese_emoticon(file_path, emoticons)
  emoticon_hash.each do |key, value|
    value.each do |value_key, value_value|
      emoticon_recieved = value_key
-     binding.pry
    end
    if emoticon_recieved = value
      emoticon_recieved
