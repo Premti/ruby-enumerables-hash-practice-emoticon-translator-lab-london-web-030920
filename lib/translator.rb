6f91b902240e534405b12c8773cead17e2599341
premti <premti@gmail.com>
2020-03-06 14:15:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ def load_library(file_path)
   #key = "angel" value = ["O:)", "japanese symbol"]]
   retrieve_emoticons.each do |key, value| 
       japanese_emoticons_hash = {key => value[1]}
-      hash[:get_meaning][japanese_emoticons_hash]
+    binding.pry
   end
   hash
 end
