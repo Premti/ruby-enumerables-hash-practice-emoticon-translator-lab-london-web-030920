e2f8db25f946ab953632585f03101ac0e5b08622
premti <premti@gmail.com>
2020-03-06 11:44:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,9 +4,10 @@ retrieve_emoticons = YAML.load('emoticons.yml')
 def load_library(file_path)
   japanese_array = []
   retrieve_emoticons.each do |file_path|
+    japanese_array.push(file_path[1])
   end
   hash = {
-    :get_meaning => {},
+    :get_meaning => {japanese_array},
     :get_emoticon => {}
   }
   hash
