82ed5ac307246f191c17492a1bd6843eda58a92e
premti <premti@gmail.com>
2020-03-06 12:04:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ retrieve_emoticons = YAML.load_file('./lib/emoticons.yml')
 binding.pry
 def load_library(file_path)
   hash = {
-    :get_meaning => {},
+    :get_meaning => {retrieve_emoticons},
     :get_emoticon => {}
   }
   hash
