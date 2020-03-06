5780600d40dcb626258abdc0dc85ff66a7e60206
premti <premti@gmail.com>
2020-03-06 14:54:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,8 @@ def load_library(file_path)
   hash
 end
 
-def get_japanese_emoticon(load_library, emoticon)
+def get_japanese_emoticon(file_path, emoticon)
+  "#{load_library}"
 end
 
 def get_english_meaning
